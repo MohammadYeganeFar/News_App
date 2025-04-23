6cf1124c32373e8d01ba7c07747a3418c9262d94
Mohammad ubuntu <mmdvafaj@gmail.com>
2025-04-23 16:12:08 +0330

    WIP

--- utils.py
+++ utils.py
@@ -1,12 +1,34 @@
 import logging
+import os
+from setting import CONTEXT_MGR_LOG_DIR
+
+def create_file_if_not_exist(path):
+    open(path, "a")
+
+create_file_if_not_exist(CONTEXT_MGR_LOG_DIR)
+
+utils_logger = logging.getLogger(__name__,)
+utils_formatter = "%(asctime)s:%(name)s:%(levelname)s:%(message)s"
+logging.basicConfig(filename=CONTEXT_MGR_LOG_DIR,
+                    format=utils_formatter,
+                    level=10)
+
+
 
 def setup_logger(*, f_path, file_handler_level:int=10,
                   stream_handler_level:int=10,
                   frmt=None,
                   name:str):
-    if frmt == None:
+
+    if not os.path.exists(f_path):
+        create_file_if_not_exist(f_path)
+        utils_logger.info(f"file not found. now created: {f_path}")
+
+
+    if frmt is None:
         frmt = "%(asctime)s:%(name)s:%(levelname)s:%(message)s"
-    if name == None:
+
+    if name is None:
         raise NameError("put a __name__")
     my_logger = logging.getLogger(name)
     my_logger.setLevel(10)
@@ -25,3 +47,4 @@ def setup_logger(*, f_path, file_handler_level:int=10,
     # my_logger.addHandler(stream_handler)
 
     return my_logger
+
